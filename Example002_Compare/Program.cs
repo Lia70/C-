47ae18c044fc85a76a819a48d8b04072236185d3
Alina Isman <you@example.com>
2023-07-13 14:35:26 +0300

    Решение задачи 2

---
+++ Example002_Compare/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает два числа,
+// а на выход выдает какое число большее, а какое меньшее
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine(a);
+else 
+    Console.WriteLine(b);
+
