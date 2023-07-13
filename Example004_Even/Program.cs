6af0010597a87ced96cec1b1dc4dd31922cca8cb
Alina Isman <you@example.com>
2023-07-13 16:43:29 +0300

    Решение задачи 6

---
+++ Example004_Even/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6. Напишите программу, которая на вход принимает число
+// выдает, является ли число четным
+
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+  Console.WriteLine("Yes");
+else
+  Console.WriteLine("No");
+
