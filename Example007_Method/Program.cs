07a4b2ff93603628889cd60bfc8242cdd879e0ac
Alina Isman <you@example.com>
2023-07-15 21:11:50 +0300

    Решение задачи

---
+++ Example007_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿//
+int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+int n = array.Length;
+int find = 41;
+int index = 0;
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+    }
+        // index=index+1;
+        index++;
+}
